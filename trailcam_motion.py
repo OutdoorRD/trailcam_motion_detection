0112bde329c86ae560b0b377fdc915d6f7cbbd32
Richard Sharp <richpsharp@gmail.com>
2016-11-25 15:41:29 -0800

    making a bounding box of all the detecting bounding boxes and some nicer reporting

--- trailcam_motion.py
+++ trailcam_motion.py
@@ -32,12 +32,12 @@ def main():
         image_index += 1
         valid, frame = video.read()
         if not valid:
-            continue
+            break
 
         masked_frame = cv2.bitwise_and(frame, mask)
         # resize the frame, convert it to grayscale, and blur it
         gray_frame = cv2.cvtColor(masked_frame, cv2.COLOR_BGR2GRAY)
-        filter_frame = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+        filter_frame = cv2.GaussianBlur(gray_frame, (31, 31), 0)
 
         if last_filter_frame is None:
             last_filter_frame = filter_frame
@@ -48,62 +48,68 @@ def main():
 
         # dilate the thresholded image to fill in holes, then find contours
         # on thresholded image
-        dilated_frame = cv2.dilate(thresholded_frame, None, iterations=2)
+        dilated_frame = cv2.dilate(thresholded_frame, None, iterations=3)
         (contour_list, _) = cv2.findContours(
             dilated_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        # loop over the contours
+        # loop over the contours and construct the largest bounding box
         detected_hiker = False
+        x, y, w, h = None, None, None, None
         for contour in contour_list:
             # if the contour is too small, ignore it
             if cv2.contourArea(contour) < MIN_CONTOUR_AREA:
                 continue
 
-            # compute the bounding box for the contour, draw it on the frame,
-            # and update the text
-            (x, y, w, h) = cv2.boundingRect(contour)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.rectangle(delta, (x, y), (x + w, y + h), (255, 255, 255), 2)
+            if x is None:
+                (x, y, w, h) = cv2.boundingRect(contour)
+                xp = x + w
+                yp = y + h
+            else:
+                (local_x, local_y, local_w, local_h) = cv2.boundingRect(
+                    contour)
+                local_xp = local_x + local_w
+                local_yp = local_y + local_h
+
+                x = min(x, local_x)
+                xp = max(xp, local_xp)
+                y = min(y, local_y)
+                yp = max(yp, local_yp)
             detected_hiker = True
 
         if detected_hiker:
+            w = xp - x
+            h = yp - y
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 2)
             image_path = os.path.join(
                 output_dir, basename + '_%d.png' % image_index)
-            print "%d,%s" % (image_index, image_path)
             cv2.imwrite(image_path, frame)
             if consecutive_start == -1:
                 consecutive_start = image_index
             n_detected += 1
         else:
             if consecutive_start != -1:
-                stats.append((consecutive_start, image_index))
-            last_filter_frame = filter_frame
-
-        #cv2.imshow("Raw Image", frame)
-        #cv2.imshow("Threshholded Delta", thresholded_frame)
-        #cv2.imshow("Frame Delta", delta)
-
-        key = cv2.waitKey(1) & 0xFF
-        # if the `q` key is pressed, break from the lop
-        if key == ord("q"):
-            break
-
-        if n_detected > 20:
-            break
+                stats.append((consecutive_start, image_index-1))
+                print 'consecutive series: %d - %d, %d' % (
+                    consecutive_start, image_index - 1,
+                    image_index - consecutive_start)
+                consecutive_start = -1
+        last_filter_frame = filter_frame
 
     video.release()
     cv2.destroyAllWindows()
 
-    with open(basename + '_stats.txt', 'w') as statsfile:
+    stats_path = os.path.join(OUTPUT_BASE_DIR, basename + '_stats.txt')
+    with open(stats_path, 'w') as statsfile:
         statsfile.write("%d frames detected\n" % n_detected)
         statsfile.write("%d series detected\n" % len(stats))
         statsfile.write("Consecutive Frame series:\n")
         for start_index, end_index in stats:
-            stats.write(
-                "%d - %d, %d" % (
-                    start_index, end_index, end_index - start_index))
-
+            statsfile.write(
+                "%d - %d, %d\n" % (
+                    start_index, end_index, end_index - start_index + 1))
+    print open(stats_path, 'r').read()
 
 
 if __name__ == '__main__':
     main()
+
