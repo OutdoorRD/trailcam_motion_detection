a7cb814ab31f45b017c4cfd33d954b8e3a2b7d74
Richard Sharp <richpsharp@gmail.com>
2016-11-25 16:25:48 -0800

    renaming of some variables and some additonal comments

--- trailcam_motion.py
+++ trailcam_motion.py
@@ -1,4 +1,4 @@
-"""Trailcam motion detection example."""
+"""Trailcam motion detection example with OpenCV and mask."""
 import os
 import time
 
@@ -6,7 +6,7 @@ import cv2
 
 MIN_CONTOUR_AREA = 4500
 VIDEO_PATH = r"C:\Users\Daddy\Downloads\160828AB.avi"
-MASK_PATH = r"E:\repositories\trailcam_motion\160828AB.avi_mask.png"
+AOI_MASK_PATH = r"E:\repositories\trailcam_motion\160828AB.avi_mask.png"
 OUTPUT_BASE_DIR = 'trailcam_detection_results'
 
 
@@ -21,12 +21,10 @@ def main():
     # Load video object
     video = cv2.VideoCapture(VIDEO_PATH)
     last_filter_frame = None
-    filter_frame = None
     image_index = -1
-    motion_in_last_frame = False
-    mask = cv2.imread(MASK_PATH)
+    aoi_mask = cv2.imread(AOI_MASK_PATH)
     consecutive_start = -1
-    stats = []
+    series_list = []
     n_detected = 0
     while True:
         image_index += 1
@@ -34,7 +32,7 @@ def main():
         if not valid:
             break
 
-        masked_frame = cv2.bitwise_and(frame, mask)
+        masked_frame = cv2.bitwise_and(frame, aoi_mask)
         # resize the frame, convert it to grayscale, and blur it
         gray_frame = cv2.cvtColor(masked_frame, cv2.COLOR_BGR2GRAY)
         filter_frame = cv2.GaussianBlur(gray_frame, (31, 31), 0)
@@ -53,43 +51,47 @@ def main():
             dilated_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         # loop over the contours and construct the largest bounding box
-        detected_hiker = False
+        detected_motion = False
         x, y, w, h = None, None, None, None
         for contour in contour_list:
             # if the contour is too small, ignore it
             if cv2.contourArea(contour) < MIN_CONTOUR_AREA:
                 continue
-
             if x is None:
+                # base bounding box
                 (x, y, w, h) = cv2.boundingRect(contour)
                 xp = x + w
                 yp = y + h
             else:
+                # calculate how this bounding box fits in with the global one
                 (local_x, local_y, local_w, local_h) = cv2.boundingRect(
                     contour)
                 local_xp = local_x + local_w
                 local_yp = local_y + local_h
-
                 x = min(x, local_x)
                 xp = max(xp, local_xp)
                 y = min(y, local_y)
                 yp = max(yp, local_yp)
-            detected_hiker = True
+            detected_motion = True
 
-        if detected_hiker:
+        if detected_motion:
+            # recalculate width and height based on BB cooridnates
             w = xp - x
             h = yp - y
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 2)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
             image_path = os.path.join(
                 output_dir, basename + '_%d.png' % image_index)
+            # save that frame to disk
             cv2.imwrite(image_path, frame)
+            # start tracking a new consecutive sequence
             if consecutive_start == -1:
                 consecutive_start = image_index
             n_detected += 1
         else:
             if consecutive_start != -1:
-                stats.append((consecutive_start, image_index-1))
-                print 'consecutive series: %d - %d, %d' % (
+                # if end of consecutive sequence, calculate the results
+                series_list.append((consecutive_start, image_index-1))
+                print 'consecutive motion series: %d - %d, %d' % (
                     consecutive_start, image_index - 1,
                     image_index - consecutive_start)
                 consecutive_start = -1
@@ -98,12 +100,13 @@ def main():
     video.release()
     cv2.destroyAllWindows()
 
+    # save a nice stats file and print result
     stats_path = os.path.join(OUTPUT_BASE_DIR, basename + '_stats.txt')
     with open(stats_path, 'w') as statsfile:
-        statsfile.write("%d frames detected\n" % n_detected)
-        statsfile.write("%d series detected\n" % len(stats))
-        statsfile.write("Consecutive Frame series:\n")
-        for start_index, end_index in stats:
+        statsfile.write("%d motion frames detected\n" % n_detected)
+        statsfile.write("%d series detected\n" % len(series_list))
+        statsfile.write("consecutive motion series:\n")
+        for start_index, end_index in series_list:
             statsfile.write(
                 "%d - %d, %d\n" % (
                     start_index, end_index, end_index - start_index + 1))
@@ -112,4 +115,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
