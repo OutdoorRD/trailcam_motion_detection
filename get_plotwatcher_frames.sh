1f65962b38be2e1f7c4a26cd3e0c76f958e2f938
Spencer Wood <Spencer Wood@localhost>
2017-10-04 07:52:46 -0700

    improved paths.
    check whether frame exists before creating one.
    
    use videos over a min size to guarantee there is a few mins of images

--- get_plotwatcher_frames.sh
+++ get_plotwatcher_frames.sh
@@ -1,14 +1,26 @@
-## extract a frame from each folder within plotwatcher/
+## extract a frame from each folder within plotwatcher
 ## this is effectively a frame from each SD card
 
-## run this from vulpes:/mnt/trails/TRAILS_STUDY_INFO
+indir="/mnt/mbs-rec/gfa_plotwatch/plotwatcher_rawvids/"
+outdir="/mnt/mbs-rec/gfa_plotwatch/plotwatcher_masks/"
 
-outdir=frames_"$(date +%m%d%y)"
-mkdir "$outdir"
+echo "Looking for videos in $indir without frames/masks..."
 
-for d in plotwatcher/*/ ;
+for d in $indir/*/ ;
 do
-	vid=$(find $d -type f -name "*.TLV" | head -1)
-	# echo "${outdir}"/"$(basename $d)"_frame.png
-	ffmpeg -i $vid -ss 00:05:00 -vframes 1 "$outdir"/"$(basename $d)"_frame.png
+  ## check for existing frame file
+  existframe="$outdir"framesNmasks/"$(basename $d)"_frame.png
+  if [ ! -e "$existframe" ]
+  then
+    ## create new frame image
+    echo "$(basename $d) doesnt exist"
+    ## find any old image (that's over 450M so it's guaranteed to have a few mins of footage)
+    vid=$(find $d -type f -size +500M -name "*.TLV" | head -1)
+    ## grab one frame from the hh:mm:ss min mark
+    echo "Grabbing frame from $vid..."
+    ffmpeg -i $vid -ss 00:01:45 -vframes 1 "$outdir"framesNmasks/"$(basename $d)"_frame.png
+  fi
 done
+
+
+
